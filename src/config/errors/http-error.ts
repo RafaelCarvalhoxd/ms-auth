db028274c4a17888d96d3f564d3fb711c272d0ab
Fael <devrafaelcarvalho14@gmail.com>
2025-07-23 15:57:14 -0300

    chore: capture sack trace and class name

--- src/config/errors/http-error.ts
+++ src/config/errors/http-error.ts
@@ -5,6 +5,8 @@ export class HttpError extends Error {
   constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
