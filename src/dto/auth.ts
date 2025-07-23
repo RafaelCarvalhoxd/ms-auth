34bdad4d603a6947ceff038391223c737c1bdcda
Fael <devrafaelcarvalho14@gmail.com>
2025-07-23 13:59:17 -0300

    feat: add auth dto

---
+++ src/dto/auth.ts
@@ -0,0 +1,14 @@
+export type LoginDto = {
+  email: string;
+  password: string;
+};
+
+export type RegisterDto = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  birthDate: Date;
+  phoneNumber: string;
+  gender: 'M' | 'F';
+};
